a017e13b49589fc206edb2b83728965e0a0b2ea0
letiziavestito <100587699+letiziavestito@users.noreply.github.com>
2024-01-10 18:38:59 +0000

    Update geneBurdenRD_visualisation_jobarray.sh

--- geneBurdenRD_visualisation_jobarray.sh
+++ geneBurdenRD_visualisation_jobarray.sh
@@ -2,7 +2,7 @@
 #BSUB -P re_gecip_enhanced_interpretation
 #BSUB -o cluster/geneBurdenRD_visualisation_jobarray_%J_%I.stdout
 #BSUB -e cluster/geneBurdenRD_visualisation_jobarray_%J_%I.stderr
-#BSUB -cwd /re_gecip/enhanced_interpretation/lvestito/gene_burden/geneBurdenRD
+#BSUB -cwd /geneBurdenRD
 #BSUB -n 5
 #BSUB -J geneBurdenRD_visualisation_jobarray[1-226]
 #BSUB -R rusage[mem=300000]
@@ -12,11 +12,11 @@ module load lang/R/4.1.0-foss-2019b
 
 # Specify 3 mandatory R arguments in quotes:
 # args[1]. job index: e.g. 'LSB_JOBINDEX'
-# args[2]. path for Exomiser masterfile file: e.g. "data/exomiser_master_file_final_01_12_23.tsv"
+# args[2]. path for Exomiser masterfile file: e.g. "data/exomiser_master_file_passvars.tsv"
 # args[3]. path for file containing list of case-control analysis labels: e.g. "data/analysisLabelList.tsv"
 
 # One optional argument
 # args[4]. choice of FDR threshold: "0.01" default is "0.05"
 # args[5]. choice "plot" or "table_only" default is "plot"
 
-Rscript /re_gecip/enhanced_interpretation/lvestito/gene_burden/geneBurdenRD/scripts/geneBurdenRD_visualisation_jobarray.R 'LSB_JOBINDEX' "/re_gecip/enhanced_interpretation/dsmedley/exomiser_master_file_final_01_12_23.tsv" "data/analysisLabelList.tsv" "0.05" "plot"
+Rscript /geneBurdenRD/scripts/geneBurdenRD_visualisation_jobarray.R 'LSB_JOBINDEX' "data/exomiser_master_file_passvars.tsv" "data/analysisLabelList.tsv" "0.05" "plot"
